83e24e0dc5dcf0869387fbc5c6055760963a5400
Muhammad Irwan Andriawan <irwan@andriawan.com>
2025-11-12 09:13:21 +0700

    ci(custom-domain): handle custom domain github pages

--- svelte.config.js
+++ svelte.config.js
@@ -19,7 +19,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.argv.includes('dev') || process.env.CUSTOM_DOMAIN ? '' : process.env.BASE_PATH
 		}
 	}
 };
