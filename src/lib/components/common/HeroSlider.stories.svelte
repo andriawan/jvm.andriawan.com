2160d2ea1d77e73dc5a63fee23b718423c3352c7
Muhammad Irwan Andriawan <irwan@andriawan.com>
2025-11-29 13:03:30 +0700

    feat: add functionality static mode to HeroSlider component

---
+++ src/lib/components/common/HeroSlider.stories.svelte
@@ -0,0 +1,21 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import HeroSlider from './HeroSlider.svelte';
+
+	const { Story } = defineMeta({
+		title: 'Landing Page/HeroSlider',
+		component: HeroSlider,
+		tags: ['autodocs'],
+		parameters: {
+			layout: 'fullscreen'
+		},
+		args: {
+			isStatic: false,
+			intervalTime: 1000
+		}
+	});
+</script>
+
+<Story name="Default">
+	<HeroSlider />
+</Story>
