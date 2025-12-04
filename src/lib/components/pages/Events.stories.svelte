f31cc99d134023cbb4ebf63348e5951f5ae761a5
Muhammad Irwan Andriawan <irwan@andriawan.com>
2025-12-04 22:04:32 +0700

    feat(events-page): add events page with filter tabs

---
+++ src/lib/components/pages/Events.stories.svelte
@@ -0,0 +1,43 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import { fn } from 'storybook/test';
+	import Events from './Events.svelte';
+
+	const { Story: EventsPageStory } = defineMeta({
+		title: 'Events Page/Main',
+		component: Events,
+		parameters: {
+			layout: 'fullscreen'
+		},
+		tags: ['autodocs'],
+		args: {
+			onMenuClick: fn()
+		}
+	});
+</script>
+
+<EventsPageStory
+	name="Default"
+	args={{
+		members: [
+			{
+				name: 'Siti Nurhaliza',
+				role: 'Co-Founder',
+				since: 2015,
+				image: 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=400&h=500&fit=crop'
+			},
+			{
+				name: 'Budi Santoso',
+				role: 'Secretary General',
+				since: 2016,
+				image: 'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?w=400&h=500&fit=crop'
+			},
+			{
+				name: 'Dewi Lestari',
+				role: 'Treasurer',
+				since: 2016,
+				image: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=400&h=500&fit=crop'
+			}
+		]
+	}}
+/>
---
+++ src/lib/components/pages/Events.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import type { Event } from '$lib/contract/Event';
+	import EventContact from '../common/EventContact.svelte';
+	import EventList from '../common/EventList.svelte';
+	import FilterTabsEvents from '../common/FilterTabsEvents.svelte';
+	import Footer from '../common/Footer.svelte';
+	import HeroEvents from '../common/HeroEvents.svelte';
+	import Navbar from '../common/Navbar.svelte';
+
+	interface Props {
+		events: Event[];
+	}
+	const { events }: Props = $props();
+</script>
+
+<Navbar />
+<HeroEvents />
+<FilterTabsEvents />
+<EventList {events} />
+<EventContact />
+<Footer />
